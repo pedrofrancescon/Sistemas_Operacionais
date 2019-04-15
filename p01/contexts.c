633c4b1b9ce581b04a07941f9089a283cd42320f
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2019-04-15 10:44:53 -0300

    Terminado projeto 01

--- p01/contexts.c
+++ p01/contexts.c
@@ -23,10 +23,12 @@ void BodyPing (void * arg)
    for (i=0; i<4; i++)
    {
       printf ("%s %d\n", (char *) arg, i) ;
+      // Troca o contexto p/ ContextPong (que irá para função BodyPong), após executar o print dessa função.
       swapcontext (&ContextPing, &ContextPong);
    }	
    printf ("%s FIM\n", (char *) arg) ;
 
+   // Quando acaba, volta p/ contexto principal do programa (ContextMain).
    swapcontext (&ContextPing, &ContextMain) ;
 }
 
@@ -41,10 +43,12 @@ void BodyPong (void * arg)
    for (i=0; i<4; i++)
    {
       printf ("%s %d\n", (char *) arg, i) ;
+      // Troca o contexto p/ ContextPing (que irá para função BodyPing), após executar o print dessa função.
       swapcontext (&ContextPong, &ContextPing);
    }
    printf ("%s FIM\n", (char *) arg) ;
 
+   // Quando acaba, volta p/ contexto principal do programa (ContextMain).
    swapcontext (&ContextPong, &ContextMain) ;
 }
 
@@ -56,12 +60,15 @@ int main (int argc, char *argv[])
 
    printf ("Main INICIO\n");
 
-   //Salva contexto atual em PING
+   // Inicializa ContextPing com o contexto atual.
    getcontext (&ContextPing);
 
+   // Aloca pilha p/ salvar informações sobre o contexto de "Ping".
    stack = malloc (STACKSIZE) ;
+   // Se a pilha existir...
    if (stack)
    {
+      // Inicializa struct ucontext_t que salva o contexto, com a respectiva pilha de memória e flags.
       ContextPing.uc_stack.ss_sp = stack ;
       ContextPing.uc_stack.ss_size = STACKSIZE;
       ContextPing.uc_stack.ss_flags = 0;
@@ -73,15 +80,20 @@ int main (int argc, char *argv[])
       exit (1);
    }
 
+   // Atribui a função BodyPing ao contexto ContextPing, dando os parâmetros com os quais a função deve ser chamada.
    makecontext (&ContextPing, (void*)(*BodyPing), 1, "    Ping");
 
    printf ("MAIN: depois de ping\n");
 
+   // Inicializa ContextPong com o contexto atual.
    getcontext (&ContextPong);
 
+   // Aloca pilha p/ salvar informações sobre o contexto de "Pong".
    stack = malloc (STACKSIZE) ;
+   // Se a pilha existir...
    if (stack)
    {
+      // Inicializa struct ucontext_t que salva o contexto, com a respectiva pilha de memória e flags.
       ContextPong.uc_stack.ss_sp = stack ;
       ContextPong.uc_stack.ss_size = STACKSIZE;
       ContextPong.uc_stack.ss_flags = 0;
@@ -93,9 +105,12 @@ int main (int argc, char *argv[])
       exit (1);
    }
 
+   // Atribui a função BodyPong ao contexto ContextPong, dando os parâmetros com os quais a função deve ser chamada.
    makecontext (&ContextPong, (void*)(*BodyPong), 1, "        Pong");
 
+   // Salva o contexto atual em ContextMain e chama o contexto ContextPing (que dispara a função BodyPing atribuida anteriormente na chamada de "makecontext").
    swapcontext (&ContextMain, &ContextPing);
+   // Chama o contexto de ContextPong uma última vez para terminar a execução do programa.
    swapcontext (&ContextMain, &ContextPong);
 
    printf ("Main FIM\n");
