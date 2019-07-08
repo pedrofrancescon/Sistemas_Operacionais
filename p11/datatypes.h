52e45219e16216f8b09ad0452f6e7ea3da94e5b0
Pedro <pedroc@alunos.utfpr.edu.br>
2019-07-07 22:18:00 -0300

    p11 Concluído

---
+++ p11/datatypes.h
@@ -0,0 +1,64 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DAINF UTFPR
+// Versão 1.0 -- Março de 2015
+//
+// Estruturas de dados internas do sistema operacional
+
+#define STACKSIZE 32768        /* tamanho de pilha das threads */
+#define _XOPEN_SOURCE 600    // para evitar erros POSIX no MacOS X
+
+#ifndef __DATATYPES__
+#define __DATATYPES__
+
+#include <ucontext.h>
+#include <signal.h>
+#include <sys/time.h>
+
+// Estrutura que define uma tarefa
+typedef struct task_t
+{
+	struct task_t *prev, *next, *joined; // para usar com a biblioteca de filas (cast)
+	int id; // ID da tarefa
+	short prioridade_estatica;
+	short prioridade_dinamica;
+	short quantum;
+	char status; //estado da tarefa
+  unsigned int init_exec_time;
+  unsigned int final_exec_time;
+  unsigned int process_time_count; //tempo que tarefa 'ficou' com processador
+	unsigned int sleep;
+  int activations;
+	ucontext_t task_context; //contexto da tarefa, não pode ser ponteiro (??)
+	int exitCode;
+
+} task_t;
+
+// estrutura que define um semáforo
+typedef struct
+{
+	struct task_t *queue;
+	int counter;
+
+
+} semaphore_t;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t;
+
+// estrutura que define uma barreira
+typedef struct
+{
+	struct task_t *queue;
+	int count;
+} barrier_t;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t;
+
+#endif
